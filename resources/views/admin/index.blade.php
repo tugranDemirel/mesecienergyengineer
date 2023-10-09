d71ff1ef7acc6f9c02375b1773be0b10d95dddf8
Tuğran Demirel <49235236+tugranDemirel@users.noreply.github.com>
2023-10-10 00:36:06 +0300

    wip

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -6,7 +6,18 @@
 @section('css')
 @endsection
 @section('content')
-    <a href="{{ route('admin.slider.index') }}">Slider</a>
+
+
+    <x-admin.pageheader name="ANASAYFA" />
+    <x-admin.alert />
+    <div class="section-wrapper">
+        <div class="row">
+            <div class="col-md-6">
+                <a href="{{ route('admin.slider.index') }}" class="btn btn-pricing">Sliderler</a>
+            </div>
+
+        </div>
+    </div>
 @endsection
 @section('js')
 @endsection
